b44e70399eeeee19eb11dcda03207aca169e1d15
mpsajulga <mychalphillipsajulga@gmail.com>
2024-11-19 21:51:51 +0800

    Added custom behavior.

--- class_car.py
+++ class_car.py
@@ -3,10 +3,16 @@ class Car:
         self.brand = brand
         self.color = color
 
+    def honk(self):
+        print("Beep beep!")
+
+
 my_car = Car('Toyota', 'Maroon')
 
 print(my_car.brand)
 print(my_car.color)
 
 
+
 print(f"The brand of my car is {my_car.brand} while the color of it is {my_car.color}.")
+my_car.honk()
